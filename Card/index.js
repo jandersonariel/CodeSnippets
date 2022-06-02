1c0676eaa9f33bda3e243ab8ef860c02a87d4813
janderson <janderson.ariel@yahoo.com.br>
2022-06-02 05:48:09 -0300

    new

--- Card/index.js
+++ Card/index.js
@@ -7,12 +7,12 @@ function Card() {
       <div className="card">
         <div className="front">
           <div className="img" ></div>
-          <h2>@janderson</h2>
+          <h2>@intagram</h2>
         </div>
         <div className="back">
           <div className="img" ></div>
-            <h2>Janderson Ariel</h2>
-            <b>@developer</b>
+            <h2>Your Name</h2>
+            <b>@intagram</b>
         </div>
       </div>
     </div>
